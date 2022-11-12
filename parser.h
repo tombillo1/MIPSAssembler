d85fada5510d456a6afbe0e2561dad4b7670ab03
Tommy Billington <tjbillington1@gmail.com>
2022-11-11 20:56:01 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -2,6 +2,11 @@
 #define ASMPARSER_H
 #include "ParseResult.h"
 
-char* parseASM(const char* const pASM);
+char* reg_to_binary(int val);
+char* imm_to_binary(int input);
+void printByte(FILE *fp, uint32_t Byte);
+char* stringToBinary(char* str);
+void parseFile(FILE *in, FILE *out, int pass);
+void parseTokens(char** beginToken, char** endToken);
 
 #endif
